4e6ae9c24be987ff3fc55bb198f0327ca1992b1d
Abdelilah <mac@iMac-de-mac.local>
2018-03-23 14:30:56 +0000

    Fix fillable field at Subscriber & Improve readme

--- src/models/Subscriber.php
+++ src/models/Subscriber.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
-    //
+
+	/**
+	*@param $fillable: contains the field that can be updated, inserted
+	*/
+
+    protected $fillable = [
+    	"name", "email"
+    ];
+
+
 }
